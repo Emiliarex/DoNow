983ca3230838b5a15dfa5841cedbc8fe22ea5015
Emily <emily@furlscrochet.com>
2015-03-09 23:03:04 +0000

    edited categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,28 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all.order("created_at ASC")
   end
 
   def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new (category-params)
+    if @category.save
+      flash[:notice] = "You just made a new category!"
+      redirect_to categories_path
+    else
+      flash[:error] = "Uh-Oh! Your category could not be saved!"
+      redirect_to :back
   end
 
   def edit
+    @category = Category.find (params [:id]) 
   end
 
   def show
+    @categories = Category.find (params [:id])
+    @todos = @categories.todos.order("deadline ASC")
   end
 end
